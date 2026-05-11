442dacefc01394fd124b2dac8c9dca2206ee0fdb
CHZXD <caiolealvargas06@gmail.com>
2026-05-11 20:00:40 -0300

    mais uma

--- backend/api-mapa/src/main/java/com/clubinho/api_mapa/CelulaController.java
+++ backend/api-mapa/src/main/java/com/clubinho/api_mapa/CelulaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/celulas")
-@CrossOrigin(origins = "*") // Permite conexões do seu frontend (GitHub Pages)
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}) // Permite conexões do seu frontend (GitHub Pages)
 public class CelulaController {
 
     @Autowired
